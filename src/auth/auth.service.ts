ef57d53eb293fc6ba441226c1b519dff47100650
Ali Ahmed <aliahmed@Alis-MacBook-Air.local>
2024-01-19 00:50:06 +0300

    add hash password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { FindAuthDto } from './dto/find-auth.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -10,13 +11,22 @@ export class AuthService {
 
   // create user
   async create(createAuthDto: CreateAuthDto): Promise<object> {
+    const { password } = createAuthDto;
+
+    // generate salt and hashed password
+    const salt = 10;
+    const genHash = await bcrypt.hash(password, salt);
+
+    // change user password with hashed password
+    createAuthDto.password = genHash;
+
     return await this.prismaService.user.create({
       data: createAuthDto,
     });
   }
 
   login(findAuthDto: FindAuthDto): object {
-    const { username, email } = findAuthDto;
+    const { username, email, password } = findAuthDto;
 
     return this.prismaService.user.findFirst({
       where: {
--- src/auth/entities/auth.entity.ts
+++ src/auth/entities/auth.entity.ts
@@ -1,13 +1,15 @@
 import { User as UserModel } from '@prisma/client';
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, ValidateIf } from 'class-validator';
 
 export class AuthEntity implements UserModel {
   id: string;
 
+  @ValidateIf((object) => !object.email)
   @IsString()
   @IsNotEmpty()
   username: string;
 
+  @ValidateIf((object) => !object.username)
   @IsEmail()
   email: string;
 
